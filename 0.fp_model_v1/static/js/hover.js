adf57a38927f8fff43ac21d864fc0a3ac66eca21
Gyoungjun Min <utilitypole97@naver.com>
2023-08-28 11:40:46 +0900

    check모델 및 관련파일_v1

---
+++ 0.fp_model_v1/static/js/hover.js
@@ -0,0 +1,33 @@
+
+// function overDash(){
+//     let image = document.getElementById('image_dash');
+//     image.setAttribute("src", "{% static 'src/dashboard.png' %}")
+// }
+// function outDash(){
+//     let image = document.getElementById('image_dash');
+//     image.setAttribute("src", "./src/dashboard_gray.png")
+// }
+// function overWebcam() {
+//     let image = document.getElementById('image_web');
+//     image.setAttribute("src", "./src/webcam.png")
+// }
+// function outWebcam(){
+//     let image = document.getElementById('image_web');
+//     image.setAttribute("src", "./src/webcam_gray.png")
+// }
+// function overStat() {
+//     let image = document.getElementById('image_stat');
+//     image.setAttribute("src", "{% static'src/statistics.png' %}")
+// }
+// function outStat(){
+//     let image = document.getElementById('image_stat');
+//     image.setAttribute("src", "./src/statistics_gray.png")
+// }
+// function overWork() {
+//     let image = document.getElementById('image_work');
+//     image.setAttribute("src", "./src/work-area.png")
+// }
+// function outWork(){
+//     let image = document.getElementById('image_work');
+//     image.setAttribute("src", "C:\div\Django_tutorial\0.connect_model\static\src/work-area_gray.png")
+// }
